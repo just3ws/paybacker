3b3a9e426c2ce96fdaa84a706663617c6e3a67fe
Mike Hall <mike@just3ws.com>
2018-12-10 22:03:03 -0600

    Removed useless spec

--- spec/paybacker_spec.rb
+++ spec/paybacker_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Paybacker do
   it 'has a version number' do
     expect(Paybacker::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
